4c3a7ab5f47cedab4ff06ae30e210bc015b4f044
Neytorokx <neytorokx@gmail.com>
2020-01-10 16:27:03 -0600

    Initial Push

--- README.md
+++ README.md
@@ -26,13 +26,23 @@ Demonstrate your understanding of this Sprint's concepts by answering the follow
 
 - [ ] What is React JS and what problems does it try and solve? Support your answer with concepts introduced in class and from your personal research on the web.
 
+React is a UI library capable of generating single page applications. It is useful for a handful of things and solves a multitude of problems, one of which being security. It blankets the code and keeps important things, such as endpoints or other crucial data, from being immediately visible to the eyes of the consumer.
+
 - [ ] What does it mean to _think_ in react?
+
+To think in react is essentially, if I were to guess, to think on a more global scope. Given that react functions as single page applications, it's important to consider each and every component or page included in the project as any could conflict with one-another.
 
 - [ ] Describe state.
 
+The state is similar to the props in a certain respect, though it refers more to the overall 'state' of the application.
+
 - [ ] Describe props.
 
+Props contain data for the component it's attached to. This is useful for passing data from one component to another, such as filling components dynamically via an API call.
+
 - [ ] What are side effects, and how do you sync effects in a React component to state or prop changes?
+
+Side effects are anything that occurs outside the main scope of the component. You can sync effects via things such as 'useEffect' or 'useState'.
 
 ## Project Set Up
 
--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,104 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from 'axios';
+import { Jumbotron } from 'reactstrap';
+import ImageCard from './components/CharacterCard';
+import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 
-const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+class App extends React.Component {
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  constructor(props) { super(props); this.state = { data: [], next: '', previous: '', search: '', LOADED: false }; }
 
-  return (
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
-    </div>
-  );
+  componentDidMount() {
+
+    // if (this.state.LOADED == false)
+      axios.get('https://swapi.co/api/people/').then(response => {
+
+        this.setState({ data: response.data.results, next: response.data.next, prev: response.data.prev, LOADED: true });
+
+      }).catch((error) => console.log(error)).finally(() => { });
+
+  }
+
+  search(event) {
+
+    this.setState({ search: event.target.value });
+
+  }
+
+  processPagination(ID) {
+
+    console.log(this.state.next);
+
+    if (ID == 0) {
+
+      axios.get(this.state.previous).then(response => {
+
+        this.setState({ data: response.data.results, next: response.data.next, prev: response.data.prev, LOADED: true });
+  
+      }).catch((error) => console.log(error)).finally(() => { });
+
+    } else {
+
+      axios.get(this.state.next).then(response => {
+
+        this.setState({ data: response.data.results, next: response.data.next, prev: response.data.prev, LOADED: true });
+  
+      }).catch((error) => console.log(error)).finally(() => { });
+
+    }
+
+  }
+
+  render() {
+
+    const cards = [];
+
+    console.log(this.state.data);
+    console.log(this.state.search);
+
+    this.state.data.forEach(result => {
+      // cards.push(<ImageCard data={result} />);
+      console.log(result.name);
+      console.log(result.name.toLowerCase().includes(this.state.search.toLowerCase()));
+      if (this.state.search != undefined && result.name.toLowerCase().includes(this.state.search.toLowerCase())) {
+        cards.push(<ImageCard data={result} />);
+      } else if (this.state.search == undefined) {
+        cards.push(<ImageCard data={result} />); 
+      }
+      console.log(cards);
+    });
+
+    if (this.state.LOADED)
+      if (this.state.data.length > 0)
+        return (
+          <div className="App" style={{ width: '100%', height: '100%' }}>
+            <form>
+              <input type='text' style={{ width: '80%' }} onChange={event => this.search(event)} />
+            </form>
+            <div style={{ width: '60%', height: '100%', margin: '20px auto 0px auto', display: 'flex', justifyContent: 'space-around', alignItems: 'center', flexWrap: 'wrap' }}>
+              {cards}
+              {/* <PaginationItem>
+                <PaginationLink previous onClick={() => {this.processPagination(0)}} />
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink next onClick={() => {this.processPagination(1)}} />
+              </PaginationItem> */}
+            </div>
+          </div>
+        );
+      else
+        return (<div className="App" style={{ width: '100%', height: '100%' }}></div>);
+    else
+      return (<>
+        <div>
+          <Jumbotron>
+            <h1 className="display-3">Loading...</h1>
+          </Jumbotron>
+        </div></>);
+
+  }
+
 }
 
 export default App;
